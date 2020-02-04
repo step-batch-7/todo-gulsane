0978587865db48153a44b61641520f0b3a0b1d9f
gulshan kumar <gulhan@123>
2020-02-04 17:59:25 +0530

    Rendering the added title on the same page

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,13 +1,23 @@
-const sendRequest = function() {
+const addTitleCard = function(title) {
+  const div = document.createElement('div');
+  div.className = 'title';
+  const h2 = document.createElement('h2');
+  h2.innerText = title;
+  div.appendChild(h2);
+  const hr = document.createElement('hr');
+  div.appendChild(hr);
+  return div;
+};
+
+const saveTitle = function() {
   const req = new XMLHttpRequest();
   req.open('POST', '/saveTitle');
   req.setRequestHeader('Content-Type', 'application/json');
   req.onload = function() {
     if (req.status === 201) {
-      const titles = document.querySelector('.titles');
-      console.log(titles);
+      const rightContainer = document.querySelector('.rightContainer');
       const title = JSON.parse(req.responseText).value;
-      titles.innerText = title;
+      rightContainer.appendChild(addTitleCard(title));
     }
   };
   const value = document.getElementById('titleInnerText').value;
