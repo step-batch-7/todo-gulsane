42b5e9589588565f38fb5e4e92ded0c77eb7b42a
gulshan kumar <gulhan@123>
2020-02-11 12:44:08 +0530

    [Gulshan|Aaquib]Added:test for ToDoListCollection add()

--- test/testToDoListCollection.js
+++ test/testToDoListCollection.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const {ToDoListCollection} = require('../lib/toDoListCollection');
+const {ToDoList} = require('../lib/toDoList');
 
 describe('ToDoListCollection', () => {
   context('load', () => {
@@ -36,6 +37,30 @@ describe('ToDoListCollection', () => {
       const toDoListCollection = ToDoListCollection.load(list);
       const expectedToDoList = undefined;
       assert.strictEqual(toDoListCollection.lastToDoList, expectedToDoList);
+    });
+  });
+
+  context('add', () => {
+    it('should create toDoList and add if toDoLists is empty', () => {
+      const toDoListCollection = ToDoListCollection.load([]);
+      toDoListCollection.add('Home Work');
+      const list = [{id: 'tl-1', title: 'Home Work', tasks: []}];
+      const expectedString = JSON.stringify(list);
+      assert.strictEqual(toDoListCollection.toJSON(), expectedString);
+      assert.ok(toDoListCollection.lastToDoList instanceof ToDoList);
+    });
+
+    it('should create toDoList and add if toDoLists is not empty', () => {
+      const availableList = [{id: 'tl-1', title: 'Home Work', tasks: []}];
+      const toDoListCollection = ToDoListCollection.load(availableList);
+      toDoListCollection.add('Class Work');
+      const list = [
+        {id: 'tl-1', title: 'Home Work', tasks: []},
+        {id: 'tl-2', title: 'Class Work', tasks: []}
+      ];
+      const expectedString = JSON.stringify(list);
+      assert.strictEqual(toDoListCollection.toJSON(), expectedString);
+      assert.ok(toDoListCollection.lastToDoList instanceof ToDoList);
     });
   });
 });
