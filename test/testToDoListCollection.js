73cab1210187d91ec67984703272176002bbbbbb
gulshan kumar <gulhan@123>
2020-02-11 18:43:38 +0530

    [Gulshan|Aaquib]Added:test for toggleTaskStatus() in ToDoListCollection

--- test/testToDoListCollection.js
+++ test/testToDoListCollection.js
@@ -125,4 +125,25 @@ describe('ToDoListCollection', () => {
       assert.deepStrictEqual(toDoListCollection.toDoLists, list);
     });
   });
+
+  context('toggleTaskStatus', () => {
+    it('should toggle the hasDone status of the given task', () => {
+      const availableList = [
+        {
+          id: 'tl-1', title: 'Home Work', tasks: [
+            {id: 'task-1', text: 'Maths', hasDone: false},
+            {id: 'task-2', text: 'English', hasDone: false}
+          ]
+        }
+      ];
+      const toDoListCollection = ToDoListCollection.load(availableList);
+      toDoListCollection.toggleTaskStatus('tl-1', 'task-1');
+      const expectedToDoList = new ToDoList('tl-1', 'Home Work', [
+        new Task('task-1', 'Maths', true),
+        new Task('task-2', 'English', false)
+      ]);
+      const list = [expectedToDoList];
+      assert.deepStrictEqual(toDoListCollection.toDoLists, list);
+    });
+  });
 });
